dd2749f47056873193b70dee0d1f47fb41c909fb
ashakirin <ashakirin@conrad.de>
2021-05-29 17:05:34 +0200

    Added transactional example

--- demo-db/src/main/java/com/example/db/DemoDbApplication.java
+++ demo-db/src/main/java/com/example/db/DemoDbApplication.java
@@ -1,15 +1,12 @@
-package com.example.db.demodb;
+package com.example.db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.example.db.entity")
-//@ComponentScan(basePackages = {"com.example.db.entity", "com.example.db.demodb"})
-@EnableJpaRepositories(basePackages = "com.example.db.entity")
 public class DemoDbApplication {
 
     public static void main(String[] args) {
