237151907d74547808a376e43addef9b6b7a7529
ashakirin <ashakirin@conrad.de>
2021-09-07 23:04:51 +0200

    Updated reactive mongodb configuration

--- spring-react/src/main/java/com/example/springreact/SpringReactApplication.java
+++ spring-react/src/main/java/com/example/springreact/SpringReactApplication.java
@@ -2,9 +2,11 @@ package com.example.springreact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories(basePackages = "com.example.springreact")
 public class SpringReactApplication {
 
     public static void main(String[] args) {
