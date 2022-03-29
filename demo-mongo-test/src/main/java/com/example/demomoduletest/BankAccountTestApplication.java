29634f25fdfa4778bcc6eeab02a6fc4a1a13d8ef
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-03-29 10:37:05 +0200

    Added tests for MongoDB

---
+++ demo-mongo-test/src/main/java/com/example/demomoduletest/BankAccountTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomoduletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountTestApplication.class, args);
+    }
+
+}
